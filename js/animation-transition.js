cb47b2e97c6bd4a97b0dd9ceca3bdae7d75f4f4a
Adam Bader <adambader25@gmail.com>
2019-12-24 13:49:13 +0000

    added transitional/animation to the cube

---
+++ js/animation-transition.js
@@ -0,0 +1,22 @@
+//Keeps selected class active/focused (ul elements)
+$('ul').on('click', function(){
+    $('ul').removeClass('selected');
+    $(this).addClass('selected');
+});
+
+//Keeps selected class active/focused (li elements)
+$('li').on('click', function(){
+    $('li').removeClass('selected');
+    $(this).addClass('selected');
+});
+
+setTimeout(function(){
+    $('.loader').fadeToggle();
+}, 1000);
+
+//Pauses page before tranisitioning to the next page
+$('.page-transition').click(function(e) {
+    e.preventDefault();
+    var linkUrl = $(this).attr('href');
+    setTimeout(function(url) { window.location = url; }, 3000, linkUrl);
+  });
