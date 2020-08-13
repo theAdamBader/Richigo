005af2b9b392293758299b42027348bf01df4444
Adam Bader <adambader25@gmail.com>
2020-08-13 10:29:35 +0100

    sound

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49379" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63189" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
