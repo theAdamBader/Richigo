4d650bdb8d959278e3318c407c5e6da98f1519a7
Adam Bader <adambader25@gmail.com>
2020-10-05 16:56:32 +0100

    amend mob margin

--- dist/preloader.78c0ad33.js
+++ dist/preloader.78c0ad33.js
@@ -168,7 +168,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61950" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49558" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
