3f8dd4281d19380fc7785fb82dec445c8ad4e5b6
Adam Bader <adambader25@gmail.com>
2020-10-05 19:29:00 +0100

    coherent colour palette

--- dist/swiper.08bec4ff.js
+++ dist/swiper.08bec4ff.js
@@ -334,7 +334,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49558" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50301" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
